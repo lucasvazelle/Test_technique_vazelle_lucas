562a7841df2de015acfbf288c01f48a5d3dd96bd
Lucasvazelle <lucasvazelle@gmail.com>
2025-06-23 22:34:44 +0200

    correciton de bugs et docker compose

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Test_technique_vazelle_lucas
+
+1. Clone le repo
+
+git clone https://github.com/TON_UTILISATEUR/test_technique_vazelle_lucas.git
+cd test_technique_vazelle_lucas
+
+
+
+2. Lancer avec Docker Compose (pull des images sur Docker Hub)
+
+$ docker compose up
+Aucune construction (build) nécessaire : Docker va automatiquement télécharger les images depuis Docker Hub.
+
+
+Deux images sont disponibles sur Docker Hub :
+
+- Backend (Ollama) : [`lucasvazelle/ollama`](https://hub.docker.com/r/lucasvazelle/ollama)  
+- Frontend (Streamlit) : [`lucasvazelle/ollama-frontend`](https://hub.docker.com/r/lucasvazelle/ollama-frontend)
+
+Accès
+- Frontend Streamlit : http://localhost:8501
+- Serving Ollama : http://localhost:11434
--- frontend/client_llm.py
+++ frontend/client_llm.py
@@ -60,3 +60,48 @@ def generate_to_streamlit(user_input, previous_context):
     r.raise_for_status()
 
     return r
+
+
+
+
+# def get_ollama_url():
+#     # Récupère le mode d'exécution ('local' ou 'docker'), défaut à 'local'
+#     mode = os.getenv("APP_ENV", "local")
+#     if mode == "docker":
+#         return "http://ollama:11434/api/generate"
+#     else:
+#         return "http://127.0.0.1:11434/api/generate"
+    
+
+# def generate(user_input, previous_context):
+#     index = load_faiss_index()
+#     retrieved_context = get_context_from_query(index, user_input)
+
+#     prompt = f"""Voici des documents utiles : \n\n
+
+# {retrieved_context}\n\n
+
+# En te basant sur ces fiches, re donne la fiche qui réponds à la question suivante, met en avant l'élément de la fiche qui permet de répondre à la question : 
+# {user_input}
+# """
+
+#     url = get_ollama_url()
+
+#     r = requests.post(
+#         url,
+#         json={
+#             "model": model,
+#             "prompt": prompt,
+#             "context": previous_context,
+#         },
+#         stream=True,
+#     )
+#     r.raise_for_status()
+
+#     for line in r.iter_lines():
+#         body = json.loads(line)
+#         print(body.get("response", ""), end="", flush=True)
+#         if "error" in body:
+#             raise Exception(body["error"])
+#         if body.get("done", False):
+#             return body["context"]
